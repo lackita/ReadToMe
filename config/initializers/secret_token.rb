a3b372ce2c251e2c24610ccdba4b0e9bf9cc8903
Colin Williams <lackita@gmail.com>
2012-02-02 06:51:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readtome::Application.config.secret_token = '9605da9a6ea84efe936f1ab16c413e014ec897f7a43ebeed4ccc3231014cc32d655127090aebe9708e5519df49e1473c4e7b20e70c67eb117a5eda04385800b6'
